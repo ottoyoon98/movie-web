4549563c0b784fa868c85cbb3553c36998bb0eaf
ottoyoon98 <ottoyoon98@gmail.com>
2022-06-08 19:38:43 +0900

    Update: 기본 뼈대 작성

---
+++ src/boxoffice.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Boxoffice()
+{
+    const toggle = React.useState(false);
+    return (
+        <div>
+            <p> today boxoffice</p>
+            
+            <p> week boxofiice </p>
+        </div>
+    )
+}
+export default Boxoffice;
